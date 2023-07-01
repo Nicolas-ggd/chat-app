abbcb8298c3ef90ab5a34503470deb221363bbcc
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-07-01 17:45:25 +0400

    add private route in routes && add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Auth } from "./components/Auth";
 import { Page404 } from "./components/404Page/404";
+import { Landing } from "./components/LandingPage/Landing";
+import { PrivateRoutes } from "./utils/PrivateRoutes";
 
 function App() {
 
   return (
     <Router>
       <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route exact path="/" element={<Landing />} />
+        </Route>
         <Route exact path="/signin" element={<Auth />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
