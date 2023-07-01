e359e6bbe65c506756b573c31c107656edf752a0
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-07-01 12:06:42 +0400

    feature: create 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Auth } from "./components/Auth";
+import { Page404 } from "./components/404Page/404";
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Auth />}/>
+        <Route exact path="/signin" element={<Auth />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </Router>
   )
