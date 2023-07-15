e0d3503ef693fa0833ca2a890d3f75ff031135ce
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-07-15 20:20:02 +0400

    remove send button

--- src/components/Chat/components/ChatBox/ChatBox.jsx
+++ src/components/Chat/components/ChatBox/ChatBox.jsx
@@ -231,9 +231,9 @@ export const ChatBox = () => {
                 <form onSubmit={sendMessage}>
                   <div className="relative w-full">
                     <input
-                      placeholder="Type message..."
+                      placeholder="Message..."
                       type="text"
-                      className="flex w-full dark:text-white dark:bg-gray-700 dark:border-gray-500 border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                      className="flex w-full dark:text-white dark:bg-gray-700 dark:border-gray-500 border rounded-3xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
                       onChange={inputTypedValue}
                       value={isMessage}
                     />
@@ -255,30 +255,6 @@ export const ChatBox = () => {
                     </button>
                   </div>
                 </form>
-              </div>
-              <div className="ml-4">
-                <button
-                  onClick={(e) => sendMessage(e)}
-                  className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
-                >
-                  <span>Send</span>
-                  <span className="ml-2">
-                    <svg
-                      className="w-4 h-4 transform rotate-45 -mt-px"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                      ></path>
-                    </svg>
-                  </span>
-                </button>
               </div>
             </div>
           </div>
